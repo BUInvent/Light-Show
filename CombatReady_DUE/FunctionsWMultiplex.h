1b8f543a667fcbbab51cf7fb7e35b2d8eaf6fab3
SlinkyMation <jmmorris2@mail.bradley.edu>
2016-08-13 13:40:15 -0500

    taking out 2's

--- CombatReady_DUE/FunctionsWMultiplex.h
+++ CombatReady_DUE/FunctionsWMultiplex.h
@@ -3,21 +3,18 @@ void wait(int);
 void AnWriteLights(int[], int);
 void count_down(int, int);
 void SingleFlash(int[]);
-//Single Flash my idea
-//void SingleFlashMult(int Light[4])
 void DoubleFlash(int, int, int);
 void DoubleFlashFinal(int, int);
 void FlashBuild(int, int);
-void bulletshot2(int[4], int[16]);
+void bulletshot(int[4], int[16]);
 void DoubleSpeedFlash(int, int);
-void preBullets2(int[4]);
+void preBullets(int[4]);
 void chorus(int, int, int);
-void intimidate2(int[4], int, int, int, int, int, int);
-void intimidate_rev2(int[4], int, int, int, int, int, int);
-void intimidate_half2(int[4], int, int, int, int, int, int);
+void intimidate(int[4], int, int, int, int, int, int);
+void intimidate_rev(int[4], int, int, int, int, int, int);
+void intimidate_half(int[4], int, int, int, int, int, int);
 void intimidate_half_sd(int, int);
-void intimidate_half_sdb(int, int, int);
-void intimidate_half_sdb2(int[4], int, int);
-void INTIMIDATE_FINAL2(int bulletsPower[4], int, int, int, int, int, int);
+void intimidate_half_sdb(int[4], int, int);
+void INTIMIDATE_FINAL(int bulletsPower[4], int, int, int, int, int, int);
 static void Play_Music( void );
 static void Stop_Music( void );
